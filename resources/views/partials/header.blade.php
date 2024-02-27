6c2eed6773f4f48f719d857174d33bf896048705
MattiaPaiano91 <147002842+MattiaPaiano91@users.noreply.github.com>
2024-02-27 13:44:39 +0100

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+@php
+    $links = [
+        [
+            'url' => '/',
+            'label' => 'Home',
+            'active' => true,
+        ],
+        [
+            'url' => '/chi-siamo',
+            'label' => 'Chi siamo',
+            'active' => true,
+        ],
+        [
+            'url' => '/contatti',
+            'label' => 'Contatti',
+            'active' => false,
+        ],
+    ];
+@endphp
+
+<header>
+    <nav>
+        <ul>
+            @foreach ($links as $link)
+                <li>
+                    <a href="{{ $link['url'] }}">
+                        {{ $link['label'] }}
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </nav>
+</header>
---
+++ resources/views/subpages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('page-title', 'Chi siamo')
+
+@section('main-content')
+@endsection
